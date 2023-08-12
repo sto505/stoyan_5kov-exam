9cf882a13dac3cc7a7ad8c309584cc76c3e00d0f
sto505 <66381889+sto505@users.noreply.github.com>
2023-08-12 16:47:46 +0200

    Update URL in boardgames.test.js

--- tests/ui/boardgames.test.js
+++ tests/ui/boardgames.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check boardgames page', async ({ page }) => {
-    await page.goto('http://localhost:8080/boardgames');
+    await page.goto('https://stoyan-5kov-exam.onrender.com/');
     const list = await page.$('ul');
     expect(list).toBeTruthy();
   });
